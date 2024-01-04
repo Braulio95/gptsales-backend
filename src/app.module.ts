486e077db7824f3c6e450c25440e7df3986d63e4
Braulio95 <braulioestradaalfonso@hotmail.com>
2024-01-03 21:59:32 -0600

    adding database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { DatabaseModule } from './core/database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), BooksModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/core/database/database.providers.ts
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+import { Sequelize } from 'sequelize-typescript';
+import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
+import { databaseConfig } from './database.config';
+//import { IDatabaseConfigAttributes } from './interfaces/dbConfig.interface'
+
+export const databaseProviders = [{
+    provide: SEQUELIZE,
+    useFactory: async () => {
+        let config: any;
+        switch (process.env.NODE_ENV) {
+            case DEVELOPMENT:
+                config = databaseConfig.development;
+                break;
+            case TEST:
+                config = databaseConfig.test;
+                break;
+            case PRODUCTION:
+                config = databaseConfig.production;
+                break;
+            default:
+                config = databaseConfig.development;
+        }
+        const sequelize = new Sequelize(config);
+        sequelize.addModels(['models goes here']);
+        await sequelize.sync();
+        return sequelize;
+    },
+}];
